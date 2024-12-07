2fc7f766d2de5bf9810301446a76d68877ddec53
Göknur <goknurgurz18@gmail.com>
2024-12-08 01:20:36 +0300

    firstpj

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('ANASAYFA');
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
